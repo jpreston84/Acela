b2549dbb0f526bedcce0f75182f4f37b5a2f9dad
jpreston84 <github@jpreston.net>
2015-04-24 15:24:33 -0400

    Database changes.

--- config/database.config.php
+++ config/database.config.php
@@ -3,17 +3,19 @@
  *  Database configuration.
  */
 
+namespace Acela\Core\Database;
+
 /**
  *  A list of all the database connections used by this application.
  */
-$GLOBALS['core']->config->database = new \stdClass();
-$GLOBALS['core']->config->database->connections = [
-	'default' => [
-		'driver'	=> 'MySQL',
-		'host'		=> 'localhost',
-		'username'	=> 'mysql_user',
-		'password'	=> 'mysql_password',
-		'database'	=> 'acela',
-		'readOnly'	=> false,
-	],
-];
+$GLOBALS['core']->config->databases = new \stdClass();
+
+/**
+ * The default MySQL database configuration.
+ */
+$GLOBALS['core']->config->databases->default = new Drivers\MySQL\Configuration;
+$GLOBALS['core']->config->databases->default->name		= 'default';
+$GLOBALS['core']->config->databases->default->host		= 'localhost';
+$GLOBALS['core']->config->databases->default->username	= 'mysql_user';
+$GLOBALS['core']->config->databases->default->password	= 'mysql_password';
+$GLOBALS['core']->config->databases->default->database	= 'acela';
--- core/classes/database/drivers/mysql/configuration.class.php
+++ core/classes/database/drivers/mysql/configuration.class.php
@@ -13,4 +13,28 @@ use Acela\Core as Core;
  */
 class Configuration extends Core\Database\Configuration
 {
+	/**
+	 * @var string $driver The name of the driver.
+	 */
+	public $driver = 'MySQL';
+	
+	/**
+	 * @var string $host The location of the host server.
+	 */
+	public $host;
+	
+	/**
+	 * @var string $username MySQL user name.
+	 */
+	public $username;
+	
+	/**
+	 * @var string $password MySQL user password.
+	 */
+	public $password;
+	
+	/**
+	 * @var string $database MySQL database name.
+	 */
+	public $database;
 }
