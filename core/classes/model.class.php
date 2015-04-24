69fdd5b462a6bf8b6137388d107da322b329a6cb
jpreston84 <github@jpreston.net>
2015-04-23 23:25:14 -0400

    Adding model template

---
+++ core/classes/model.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Acela\Core;
+
+/**
+ *  A template for data models.
+ */
+abstract class Model
+{
+	/**
+	 *  Create a new instance of the model.
+	 */
+	function __construct()
+	{
+		return;
+	}
+}
