b853138bf709ae2b9c59267314160338dacf624f
jpreston84 <github@jpreston.net>
2015-04-24 12:12:28 -0400

    Added core.php

---
+++ core/core.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *  Acela - A PHP Rapid Development Framework
+ *  
+ *  Main file.
+ */
+
+/**
+ *  Core object that contains references to class instances and holds
+ *  configuration data.
+ */
+$core = new \stdClass;
