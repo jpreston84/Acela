51f0bcb98e9a5988a3b4d5a617fda69b58bd0697
jpreston84 <github@jpreston.net>
2015-04-24 13:02:47 -0400

    Doc changes.

--- core/core.php
+++ core/core.php
@@ -1,20 +1,20 @@
 <?php
 /**
- *  Acela - A PHP Rapid Development Framework
- *  
- *  Main file.
+ * Acela - A PHP Rapid Development Framework
+ * 
+ * Main file.
  */
 
 /**
- *  Core object that contains references to class instances and holds
- *  configuration data.
+ * Core object that contains references to class instances and holds
+ * configuration data.
  *  
- *  @global \stdClass $GLOBALS['core']
- *  @name $core
+ * @global \stdClass $GLOBALS['core']
+ * @name $core
  */
 $GLOBALS['core'] = new \stdClass;
 
 /**
- *  Include the global configuration file.
+ * Include the global configuration file.
  */
 require_once __DIR__.'/../config/config.php';
