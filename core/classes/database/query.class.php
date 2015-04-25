f09a92b327239e757d83713d29e3b94910fa0a47
jpreston84 <github@jpreston.net>
2015-04-25 14:09:16 -0400

    Database changes.

--- core/classes/database/query.class.php
+++ core/classes/database/query.class.php
@@ -15,9 +15,24 @@ abstract class Query
 	 * 
 	 * @param string $name The name of the table to add.
 	 * @param string $alias The alias to use for this table.
-	 * @return this A reference to the current object.
+	 * @return self A reference to the current object.
 	 */
 	public function table($name, $alias)
+	{
+		return $this;
+	}
+
+	/**
+	 * Add a join condition for the most recently added table.
+	 * 
+	 * @param string $field1 The name or identity of the first field in the join condition.
+	 * @param string $condType The type of join condition.
+	 * @param string $field2 The identity of the second field in the join condition.
+	 * @return self A reference to the current object.
+	 * 
+	 * @description If one parameter is specified, it will be treated as a field that must match between both tables. If more parameters are specified, they will identify the fields in each table, and the exact type of conditional (equal, lt, gt).
+	 */
+	public function cond($field1, $condType, $field2)
 	{
 		return $this;
 	}
