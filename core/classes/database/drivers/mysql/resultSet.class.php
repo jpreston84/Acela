51240c02b3df2d7260b5a0c9619c7ad5a5bb9b10
jpreston84 <github@jpreston.net>
2015-04-24 22:23:52 -0400

    Database changes.

--- core/classes/database/drivers/mysql/resultSet.class.php
+++ core/classes/database/drivers/mysql/resultSet.class.php
@@ -12,4 +12,39 @@ use \Acela\Core\Database as Database;
  */
 class ResultSet extends Database\ResultSet
 {
+	/**
+	 * @var \PDOStatement $res Resource for the result set.
+	 */
+	private $res;
+	
+	/**
+	 * Instantiate the result set and save the PDO resource.
+	 * 
+	 * @param \PDOStatement $res The PDO statement result set that will be used.
+	 */
+	function __construct($res)
+	{
+		$this->res = $res;
+	}
+	
+	/**
+	 * Get one or more rows of data from the record set.
+	 * 
+	 * @param int $qty The number of rows to retrieve. Default 1.
+	 * @return array|false An associative array containing the names of columns as array keys and values of columns as array values.
+	 */
+	function get($qty = 1)
+	{
+		return $res->fetch();
+	}
+
+	/**
+	 * Get all rows of data from the record set.
+	 * 
+	 * @return array|false An multidimensional array containing all of the records from the data set, stored as associative arrays.
+	 */
+	function getAll()
+	{
+		return $res->fetchAll();
+	}
 }
