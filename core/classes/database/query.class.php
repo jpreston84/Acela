2a95371821bb35e064223df77ba4aa369562e2f1
jpreston84 <github@jpreston.net>
2015-04-25 15:03:11 -0400

    Doc changes.

--- core/classes/database/query.class.php
+++ core/classes/database/query.class.php
@@ -36,12 +36,13 @@ abstract class Query
 	/**
 	 * Add a join condition for the most recently added table.
 	 * 
+	 * If one parameter is specified, it will be treated as a field that must match between both tables. If more parameters are specified, they will identify the fields in each table, and the exact type of conditional (equal, lt, gt).
+	 * 
 	 * @param string|array $field1 The name or identity of the first field in the join condition.
 	 * @param string|null $condType The type of join condition.
 	 * @param array|null $field2 The identity of the second field in the join condition.
 	 * @return self A reference to the current object.
 	 * 
-	 * @description If one parameter is specified, it will be treated as a field that must match between both tables. If more parameters are specified, they will identify the fields in each table, and the exact type of conditional (equal, lt, gt).
 	 */
 	public function cond($field1, $condType = null, $field2 = null)
 	{
