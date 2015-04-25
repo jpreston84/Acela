9bcd936eaf33e8c9c679ea08368be540aa199f93
jpreston84 <github@jpreston.net>
2015-04-25 15:00:35 -0400

    Database changes.

--- core/classes/database/query.class.php
+++ core/classes/database/query.class.php
@@ -11,6 +11,12 @@ namespace Acela\Core\Database\Drivers;
 abstract class Query
 {
 	/**
+	 * @var array $tables List of tables in this query.
+	 */
+	private $tables = [];
+	private 
+	
+	/**
 	 * Add a new table to the query.
 	 * 
 	 * @param string $name The name of the table to add.
@@ -19,6 +25,12 @@ abstract class Query
 	 */
 	public function table($name, $alias)
 	{
+		$this->tables[] = [
+			'name' => $name,
+			'alias' => $alias,
+			'conditions' => [
+			],
+		];
 		return $this;
 	}
 
@@ -26,14 +38,16 @@ abstract class Query
 	 * Add a join condition for the most recently added table.
 	 * 
 	 * @param string|array $field1 The name or identity of the first field in the join condition.
-	 * @param string $condType The type of join condition.
-	 * @param array $field2 The identity of the second field in the join condition.
+	 * @param string|null $condType The type of join condition.
+	 * @param array|null $field2 The identity of the second field in the join condition.
 	 * @return self A reference to the current object.
 	 * 
 	 * @description If one parameter is specified, it will be treated as a field that must match between both tables. If more parameters are specified, they will identify the fields in each table, and the exact type of conditional (equal, lt, gt).
 	 */
-	public function cond($field1, $condType, $field2)
+	public function cond($field1, $condType = null, $field2 = null)
 	{
+		
+		$this->tables[key($this->tables)]['conditions'];
 		return $this;
 	}
 }
