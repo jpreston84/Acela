f0b377f931d995391c1c0dfd371f90c31c4cd40d
jpreston84 <github@jpreston.net>
2015-04-27 18:18:54 -0400

    Database fixes.

--- core/classes/database/engine.class.php
+++ core/classes/database/engine.class.php
@@ -19,7 +19,7 @@ class Engine
 	 * Instantiate the database handler and connect to the specified source.
 	 * @param DatabaseEngineConfiguration $config A database configuration.
 	 */
-	public function __construct(Configuration $config)
+	public function __construct(Drivers\Configuration $config)
 	{
 		/**
 		 * Load the driver for this engine instance and pass it appropriate
