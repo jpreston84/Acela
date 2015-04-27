82dc1b659820a6c996e9047b2f7615c8d8eb2824
jpreston84 <github@jpreston.net>
2015-04-27 18:12:04 -0400

    Added autoloader.

--- core/core.php
+++ core/core.php
@@ -17,10 +17,15 @@ namespace Acela\Core;
 $GLOBALS['core'] = new \stdClass;
 
 /**
+ * Load the autoloader functions.
+ */
+require_once __DIR__.'/functions/autoload.functions.php';
+spl_autoload_register('autoloadClasses');
+
+/**
  * Include the global configuration file.
  */
 require_once __DIR__.'/../config/config.php';
-
 
 /**
  * Load the database engine.
---
+++ core/functions/autoload.functions.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Autoload functions.
+ */
+
+namespace Acela\Core;
+
+ /**
+ * A function to autoload classes.
+ * 
+ * @param string $class The name of the class that needs to be loaded.
+ */
+function autoloadClasses($class)
+{
+	/**
+	 * Break the class name into path components.
+	 */
+	$classNameComponents = explode('\\', $class);
+
+	/**
+	 * Look for an appropriate class file in the Acela Core.
+	 */
+	if(
+		$classNameComponents[0] == 'Acela'
+	)
+	{
+		array_shift($classNameComponents); // Shift off Acela.
+
+		if($classNameComponents[0] == 'Core')
+		{
+			$classNameComponents = array_merge( // Add 'classes' as the second element of the file path.
+				array_slice($classNameComponents, 0, 1),
+				['classes'],
+				array_slice($classNameComponents, 1)
+			);
+			
+			$classNameComponents[key($classNameComponents)] .= '.class.php';
+			
+			foreach($classNameComponents as $num => $component)
+			{
+				$classNameComponents[$num][0] = strtoupper($component[0]);
+			}
+			
+			require_once __DIR__.'/../'.implode('/', $classNameComponents);
+		}
+	}
+}
