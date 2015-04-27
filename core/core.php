003fe38ab3b01a5cfe609694ecf8b98c723157cd
jpreston84 <github@jpreston.net>
2015-04-27 14:51:50 -0400

    Database changes.

--- core/core.php
+++ core/core.php
@@ -21,8 +21,10 @@ $GLOBALS['core'] = new \stdClass;
  */
 require_once __DIR__.'/../config/config.php';
 
+
 /**
  * Load the database engine.
  */
 require_once __DIR__.'/classes/database/engine.class.php';
+require_once __DIR__.'/../config/database.config.php';
 $core->db = new Database\Engine($core->config->databases->default);
