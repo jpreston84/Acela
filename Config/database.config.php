09fd518f7ec5407ae6ca5da1b772b22437f02cb9
jpreston84 <github@jpreston.net>
2015-05-02 14:29:11 -0400

    Fixing database config.

--- Config/database.config.php
+++ Config/database.config.php
@@ -8,14 +8,14 @@ namespace Acela\Core\Database;
 /**
  *  A list of all the database connections used by this application.
  */
-$GLOBALS['core']->config->databases = new \stdClass();
+$GLOBALS['core']->config->database = new \stdClass();
 
 /**
  * The default MySQL database configuration.
  */
-$GLOBALS['core']->config->databases->default			= new Drivers\MySQL\Configuration;
-$GLOBALS['core']->config->databases->default->name		= 'default';
-$GLOBALS['core']->config->databases->default->host		= 'localhost';
-$GLOBALS['core']->config->databases->default->username	= 'acela';
-$GLOBALS['core']->config->databases->default->password	= 'password';
-$GLOBALS['core']->config->databases->default->database	= 'acela';
+$GLOBALS['core']->config->database->default			= new Drivers\MySQL\Configuration;
+$GLOBALS['core']->config->database->default->name		= 'default';
+$GLOBALS['core']->config->database->default->host		= 'localhost';
+$GLOBALS['core']->config->database->default->username	= 'acela';
+$GLOBALS['core']->config->database->default->password	= 'password';
+$GLOBALS['core']->config->database->default->database	= 'acela';
