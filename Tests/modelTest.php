9d610b2991b34ee5637a812bafba8fbc2e7a5528
jpreston84 <github@jpreston.net>
2015-05-02 22:35:59 -0400

    Model test changes.

--- Tests/modelTest.php
+++ Tests/modelTest.php
@@ -10,5 +10,15 @@ use \Acela\Core as Core;
 require_once __DIR__.'/../Core/Core.php';
 
 $userManager = Core\Model::get('User');
+$userManager->name = 'Instance 1';
+
+print_r($userManager);
+
+$userManager = new Core\Models\User\Manager;
+$userManager->name = 'Instance 2';
+
+print_r($userManager);
+
+$userManager = Core\Model::get('User');
 
 print_r($userManager);
