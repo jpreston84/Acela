9cd23bc26e1f02e9d4067902cf94fef30254594f
jpreston84 <github@jpreston.net>
2015-05-09 16:51:56 -0400

    Model test changes.

--- Tests/modelTest.php
+++ Tests/modelTest.php
@@ -11,7 +11,13 @@ require_once __DIR__.'/../Core/Core.php';
 
 $userManager = Core\Model::getInstance('User');
 
-$user = $userManager->getFirst( [] );
+$user = $userManager->create();
+$user->firstName = 'Jonathan';
+$user->lastName = 1;
 
-$user->firstName .= ' - Modified';
-$user->save();
+for($i = 1; $i <= 10; $i++)
+{
+	$user = $userManager->getFirst( [ 'firstName' => 'Jonathan' ] );
+	$user->lastName++;
+	$user->save();
+}
