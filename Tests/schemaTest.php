c8630eb28fad0060ca58e3c98215b4bbebfd7918
jpreston84 <github@jpreston.net>
2015-05-20 22:45:07 -0400

    Bug fix.

--- Tests/schemaTest.php
+++ Tests/schemaTest.php
@@ -3,10 +3,13 @@
  * A test for the new Schema object.
  */
 
+namespace Acela\Application;
+
+use \Acela\Core;
  
 require_once __DIR__.'/../Core/Core.php';
 
-$table = Schema::get('users');
+$table = Core\Schema::get('users');
 
 echo '<pre>';
 foreach($table as $field)
